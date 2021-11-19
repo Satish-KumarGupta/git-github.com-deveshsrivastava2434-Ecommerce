67fb8c0b953808097a9c5a6f1950e7cb76f60aff
satish122 <53410112+satish122@users.noreply.github.com>
2021-11-19 14:54:35 +0530

    Fix on nav

--- src/Component/pages/header.js
+++ src/Component/pages/header.js
@@ -5,136 +5,49 @@ export default function Header() {
     return (
     <div>
     {/* <!-- Header Section Begin --> */}
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-6">
-                        <div class="header__top__left">
-                            <ul>
-                                <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
-                                <li>Free Shipping for all Order of $99</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6">
-                        <div class="header__top__right">
-                            <div class="header__top__right__social">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-linkedin"></i></a>
-                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
-                            </div>
-                            <div class="header__top__right__language">
-                                <img src="img/language.png" alt=""/>
-                                <div>English</div>
-                                <span class="arrow_carrot-down"></span>
-                                <ul>
-                                    <li><a href="#">Spanis</a></li>
-                                    <li><a href="#">English</a></li>
-                                </ul>
-                            </div>
-                            <div class="header__top__right__auth">
-                                <a href="#"><i class="fa fa-user"></i> Login</a>
-                            </div>
-                        </div>
-                    </div>
+    <header className="header">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-3">
+                    <nav className="header__logo">
+                        <Link to="/"><img src="img/logo.png" alt=""/></Link>
+                    </nav>
                 </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="header__logo">
-                        <a href="./"><img src="img/logo.png" alt=""/></a>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <nav class="header__menu">
-                        <ul>
-                            <li class="active"><a href="./home">Home</a></li>
-                            <li><a href="./shop">Shop</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="header__menu__dropdown">
-                                    <li><a href="./shop-details.html">Shop Details</a></li>
-                                    <li><a href="./shoping-cart.html">Shoping Cart</a></li>
-                                    <li><a href="./checkout.html">Check Out</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                <div className="col-lg-6">
+                    <nav className="header__menu">
+                        <ul >
+                            <li className="active"><Link to="/home"  className="nav-link">Home</Link></li>
+                            <li><Link to="/shop">Shop</Link></li>
+                            <li><Link to="#">Pages</Link>
+                                <ul className="header__menu__dropdown">
+                                    <li><Link to="./#">Shop Details</Link></li>
+                                    <li><Link to="./#">Shoping Cart</Link></li>
+                                    <li><Link to="./#">Check Out</Link></li>
+                                    <li><Link to="./#">Blog Details</Link></li>
                                 </ul>
                             </li>
-                            <li><a href="./blog.html">Blog</a></li>
-                            <li><a href="./contact">Contact</a></li>
+                            <li><Link to="#">Blog</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
                         </ul>
                     </nav>
                 </div>
-                <div class="col-lg-3">
-                    <div class="header__cart">
+                <div className="col-lg-3">
+                    <div className="header__cart">
                         <ul>
-                            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
-                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+                            <li><Link to="#"><i className="fa fa-heart"></i> <span>1</span></Link></li>
+                            <li><Link to="#"><i className="fa fa-shopping-bag"></i> <span>3</span></Link></li>
                         </ul>
-                        <div class="header__cart__price">item: <span>$150.00</span></div>
+
                     </div>
                 </div>
             </div>
-            <div class="humberger__open">
-                <i class="fa fa-bars"></i>
+            <div className="humberger__open">
+                <i className="fa fa-bars"></i>
             </div>
         </div>
     </header>
         {/* <!-- Header Section End --> */}
-        {/* <!-- Hero Section Begin --> */}
-    <section class="hero hero-normal">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="hero__categories">
-                        <div class="hero__categories__all">
-                            <i class="fa fa-bars"></i>
-                            <span>All departments</span>
-                        </div>
-                        <ul>
-                            <li><a href="#">Fresh Meat</a></li>
-                            <li><a href="#">Vegetables</a></li>
-                            <li><a href="#">Fruit & Nut Gifts</a></li>
-                            <li><a href="#">Fresh Berries</a></li>
-                            <li><a href="#">Ocean Foods</a></li>
-                            <li><a href="#">Butter & Eggs</a></li>
-                            <li><a href="#">Fastfood</a></li>
-                            <li><a href="#">Fresh Onion</a></li>
-                            <li><a href="#">Papayaya & Crisps</a></li>
-                            <li><a href="#">Oatmeal</a></li>
-                            <li><a href="#">Fresh Bananas</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <div class="hero__search__categories">
-                                    All Categories
-                                    <span class="arrow_carrot-down"></span>
-                                </div>
-                                <input type="text" placeholder="What do yo u need?"/>
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+65 11.188.888</h5>
-                                <span>support 24/7 time</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    {/* <!-- Hero Section End --> */}
+      
 </div>
     )
 }
